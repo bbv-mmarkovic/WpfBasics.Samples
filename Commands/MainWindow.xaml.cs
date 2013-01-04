3bd3b93acd6c91f1dd2d0501a65b1d6dec75d4d7
Marko Markovic <marko.markovic@bbv.ch>
2013-01-04 19:00:10 +0100

    WPF Features: Added Commands example.

---
+++ Commands/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Commands
+{
+    using System.Windows;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ICommand MyCommand { get; private set; }
+
+        public MainWindow()
+        {
+            this.MyCommand = new ShowInputCommand();
+            this.DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ Commands/ShowInputCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace Commands
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class ShowInputCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return parameter is string && !string.IsNullOrEmpty((string)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+
+            MessageBox.Show((string)parameter);
+        }
+    }
+}
